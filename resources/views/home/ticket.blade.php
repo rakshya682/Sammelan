f5ee797e9182f290e810fc9e8a43a6a31475fa6f
coolayushap <payush637@gmail.com>
2023-07-21 16:42:03 +0545

    final

--- resources/views/home/ticket.blade.php
+++ resources/views/home/ticket.blade.php
@@ -1,6 +1,11 @@
 
-<a href='http://127.0.0.1:8000/images/ticket.jpg' download>
-    Download
-</a>
+<div style="margin-top:150px;margin-left:450px;">
 
-<img style='width:50%;margin:0 auto;' src='http://127.0.0.1:8000/images/ticket.jpg'>
+<img style="width:100%;margin-left:-200px; auto;
+height: auto;" src='http://127.0.0.1:8000/images/ticket.jpg'>
+
+  </div>
+  <button type="button" style="margin-top:20px;margin-left:600px;color: #FFFFFF;
+    text-decoration: none; font-size:20px; "><a style="text-decoration: none;   color:black;"href='http://127.0.0.1:8000/images/ticket.jpg' download>
+    <div><h3>Download The Ticket:</h3></div>
+</a></button>
