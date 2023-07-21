66a34e51d6334643b4e516f36852706ab9eca1f4
iamnishan10 <princepiano66@gmail.com>
2023-07-21 17:02:01 +0545

    logo update

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,4 +1,6 @@
-<a href="/">
-<img src="images/logo.png" alt="This is an image">
-
+<div style="height:100px;width:20%; margin-left:500px;padding-bottom 100px; "><a href="/">
+    <div style="margin-top:-10px;">
+<img  src="images/mainlogo.png" alt="This is an image">
+</div>
 </a>
+</div>
